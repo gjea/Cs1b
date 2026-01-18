cc39d4eb282f49720d6ca959f3eaf2d9939af880
gjea <gaeje7@gmail.com>
2026-01-18 15:44:41 -0800

    update comments and bullshit

--- HW_1b/source.cpp
+++ HW_1b/source.cpp
@@ -1,20 +1,46 @@
+// Attached: HW 1(a-e)
+// File: HW_1b.pdf
+//
+//
+// Programmer: Gage Alvarez
+// Class: CS 1B
+// Instructor: Med Mogasemi
+//
+//
+// Program: Display Average Temperature
+//
+// Description:
+// After inputting three Temperatures, program finds the average and displays it
+//
+//
 #include <iostream>
-
+// function prototypes
 void getTemps(float &temp1, float &temp2, float &temp3);
 float calcAvg(float temp1, float temp2, float temp3);
 void displayAvg(float temp1, float temp2, float temp3, float average);
 
+// main
+
 int main() {
+    // initialize variables
     float temp1 = 0;
     float temp2 = 0;
     float temp3 = 0;
     float average = 0;
+    // function to retrieve all 3 Temperatures
     getTemps(temp1, temp2, temp3);
+    // function to calculate the average of the three temperatures
     average = calcAvg(temp1, temp2, temp3);
+    // function to display the average and the three temperatures
     displayAvg(temp1, temp2, temp3, average);
     return 0;
 }
 
+// getTemps
+// function accepts input 3 times for the three temperatures
+// Input: temperature variables and user input
+//
+// Output: temperature variables
 void getTemps(float &temp1, float &temp2, float &temp3) {
     std::cout << "Enter temp 1: " << '\n';
     std::cin >> temp1;
@@ -24,10 +50,23 @@ void getTemps(float &temp1, float &temp2, float &temp3) {
     std::cin >> temp3;
 }
 
+// calcAvg
+// function calculates the average of the three temperatures
+// returns value to main
+// Input: 3 temp variables
+//
+// Output: average of temp variables
 float calcAvg(float temp1, float temp2, float temp3) {
     return (temp1 + temp2 + temp3) / 3;
 }
 
+// displayAvg
+// function displays all important values such as average, and the three
+// temperatures
+//
+// input: 3 temp variables and average
+//
+// Output: displays values to console
 void displayAvg(float temp1, float temp2, float temp3, float average) {
     std::cout << "City #1 temp" << temp1 << '\n';
     std::cout << "City #2 temp" << temp2 << '\n';
